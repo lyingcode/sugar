3152699ae85e2ed693cb416502957781289e4403
lyingcode <jaledi@qq.com>
2022-10-01 08:53:32 +0800

    feat:增加cache示例

---
+++ src/main/java/com/bitsoft/sugar/cache/GuavaCache.java
@@ -0,0 +1,49 @@
+package com.bitsoft.sugar.cache;
+
+import com.google.common.base.Strings;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class GuavaCache {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        LoadingCache<String, Object> cache = CacheBuilder.newBuilder()
+                .maximumSize(10)
+                .expireAfterWrite(3, TimeUnit.SECONDS)
+                .recordStats()
+                .removalListener(notification -> System.out.println(notification.getKey() + ":" + notification.getCause()))
+                .build(new CacheLoader<>() {
+                    @Override
+                    public Object load(String key) throws Exception {
+                        return Thread.currentThread().getName() + ":" + key;
+                    }
+                });
+        initCache(cache);
+        System.out.println("cache.size():" + cache.size());
+        displayCache(cache);
+        Thread.sleep(1000);
+        System.out.println(Strings.repeat("=",20));
+        System.out.println("是否存在:" + cache.getIfPresent("17"));
+        Thread.sleep(2500);
+        System.out.println(Strings.repeat("=",20));
+        System.out.println("休息后是否存在:" + cache.getIfPresent("17"));
+        displayCache(cache);
+    }
+
+    public static void initCache(LoadingCache<String, Object> cache) throws ExecutionException {
+        for (int i = 0; i < 20; i++) {
+            cache.get(String.valueOf(i));
+        }
+    }
+
+    public static void displayCache(LoadingCache cache) {
+        Iterator its = cache.asMap().entrySet().iterator();
+        while (its.hasNext()) {
+            System.out.println(its.next().toString());
+        }
+    }
+}
